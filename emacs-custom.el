e23a39d6f20053897aecea7ca3b516b498a35b43
cratzdo <hongrengong@gmail.com>
2017-05-19 21:47:47 -0400

    add epy-mode

--- emacs-custom.el
+++ emacs-custom.el
@@ -10,13 +10,33 @@
    [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
    (vector "#839496" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"))
- '(custom-enabled-themes (quote (sanityinc-solarized-light)))
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 308 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
  '(custom-safe-themes
    (quote
     ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(default-frame-alist
+    (quote
+     ((tool-bar-lines . 0)
+      (menu-bar-lines . 1)
+      (foreground-color . "Black")
+      (background-color . "White")
+      (cursor-type . box)
+      (cursor-color . "Red")
+      (internal-border-width . 0)
+      (left-fringe . 1)
+      (right-fringe)
+      (fringe)
+      (vertical-scroll-bars))))
+ '(display-time-mode t)
  '(fci-rule-color "#073642")
+ '(global-hl-line-mode t)
  '(inhibit-startup-screen t)
  '(matlab-shell-command-switches (quote ("-nodesktop -nosplash")))
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode nil t)
  '(package-selected-packages
    (quote
     (flyspell-correct-helm writeroom-mode use-package smartscan smartparens smart-cursor-color pretty-mode peep-dired osx-dictionary org-bullets matlab-mode langtool helm google-this ess company-auctex color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized auto-save-buffers-enhanced auto-compile)))
@@ -41,7 +61,8 @@
      (320 . "#6c71c4")
      (340 . "#dc322f")
      (360 . "#cb4b16"))))
- '(vc-annotate-very-old-color nil))
+ '(vc-annotate-very-old-color nil)
+ '(visual-line-mode nil t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
